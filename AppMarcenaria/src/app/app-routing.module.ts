67f1c22fa802d4e380f8d46dea078309a1411c19
Daywid <88506180+daywid@users.noreply.github.com>
2023-11-20 15:40:09 -0300

    Adicionando Cliente e Fornecedores

--- AppMarcenaria/src/app/app-routing.module.ts
+++ AppMarcenaria/src/app/app-routing.module.ts
@@ -3,16 +3,24 @@ import { RouterModule, Routes } from '@angular/router';
 import { AgendasComponent } from './components/agendas/agendas.component';
 import { FuncionariosComponent } from './components/funcionarios/funcionarios.component';
 
-// ADICIONANDO COMPONENTES DO ESTOQUE, MATERIAL, PROJETO E TAREFA 
+// import { ClientesComponent } from './components/clientes/clientes.component';
+// // import { FornecedoresComponent } from './components/fornecedores/fornecedores.component';
+
+// ADICIONANDO COMPONENTES DO ESTOQUE, MATERIAL, PROJETO E TAREFA
 import { EstoquesComponent } from './components/estoques/estoques.component';
 import { MateriaisComponent } from './components/materiais/materiais.component';
 import { ProjetosComponent } from './components/projetos/projetos.component';
 import { TarefasComponent } from './components/tarefas/tarefas.component';
 
+
+
 const routes: Routes = [
   {path: 'agendas', component:AgendasComponent},
   {path: 'funcionarios', component:FuncionariosComponent},
-  
+
+  // {path:'fornecedor', component:FornecedoresComponent},
+  // {path:'cliente', component:ClientesComponent},
+
   // ADICIONANDO ROTAS DE ESTOQUE, MATERIAL, PROJETO E TAREFA
   {path: 'estoque', component:EstoquesComponent},
   {path: 'materiais', component:MateriaisComponent},
